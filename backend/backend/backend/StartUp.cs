1b8f81f9c771df2c91fd98497f1a3fafb8381295
Raevsk1i <raevski_yt@mail.ru>
2025-02-27 09:48:04 +0300

    Init backend for user

---
+++ backend/backend/backend/StartUp.cs
@@ -0,0 +1,49 @@
+using backend.controllers;
+using Microsoft.AspNetCore.Builder;
+
+namespace backend
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    public record Startup(IConfiguration Configuration)
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ backend/backend/backend/controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.controllers
+{
+    [Route("/api")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public int get1()
+        {
+            return 1;
+        }
+
+    }
+}
